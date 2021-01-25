970a033627512edb081cede3eb05664c07086d16
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2021-01-25 13:34:03 +0300

    of()
    of operatörü içerisine verdiğimiz değerleri bir observable olarak döner.Subscribe olup değerleri okuyabiliriz.

--- creationOperators/ofOperator.ts
+++ creationOperators/ofOperator.ts
@@ -1,1 +1,19 @@
+import { Component } from '@angular/core';
+import {of} from 'rxjs'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  constructor(){
+    //of operatörü içerisine verdiğimiz değerleri bir observable olarak döner.Subscribe olup değerleri okuyabiliriz.
+    const values = of("Melike",[1,2,3],45,4.54);
+    values.subscribe(data=>{
+      console.log(data)
+    })
+
+    
+}
 
